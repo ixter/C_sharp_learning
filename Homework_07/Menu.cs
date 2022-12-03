84dc3de6ed7c78a4c751fb92da481ec871af9411
ixter <ixter08@gmail.com>
2022-12-03 16:45:51 +0300

    7 домашка

---
+++ Homework_07/Menu.cs
@@ -0,0 +1,28 @@
+﻿namespace Homework_07;
+
+public class Menu
+{
+    /// <summary>
+    ///  Функция отрисовки меню с получаемыми параметрами
+    /// </summary>
+    /// <param name="items"></param>
+    /// <param name="row"></param>
+    /// <param name="col"></param>
+    /// <param name="index"></param>
+    public static void DrawMenu(string[] items, int row, int col, int index)
+    {
+        Console.SetCursorPosition(col, row);
+        for (int i = 0; i < items.Length; i++)
+        {
+            if (i == index)
+            {
+                Console.BackgroundColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+            Console.WriteLine(items[i]);
+            Console.ResetColor();
+        }
+
+        Console.WriteLine();
+    }
+}
